b8ebe0294464e843091d4bea2aefd53ecc7c4925
Hamz4428 <94164892+Hamz4428@users.noreply.github.com>
2021-11-26 08:42:40 +0100

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -4,23 +4,23 @@ from bs4 import BeautifulSoup
 import csv
 from urllib.request import Request, urlopen
 
-#connect to db
+#konnekte to db
 password = input("Enter Password: ")
 conn = pymysql.connect(host = 'localhost', user='root', passwd=password,db='mysql')
 cur = conn.cursor()
 cur.execute("USE aquarium")
 
-#drop table if currently exists
+#drop table  vis det allerede finnes
 delete_table = "DROP TABLE IF EXISTS `Fish`;" 
 cur.execute(delete_table)
 conn.commit()
 
-#create table to store data
+#lager table for å lagre data
 create_table ="CREATE TABLE `Fish` ( `fish_id` int NOT NULL AUTO_INCREMENT, `name` varchar(50) NOT NULL, `genus` varchar(50), `species` varchar(50), PRIMARY KEY (`fish_id`) ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
 cur.execute(create_table)
 conn.commit()
 
-#set up beautiful soup for web scraping
+#seter up beautiful soup for web scraping
 site ='https://dwazoo.com/exhibit/aquarium/page/'
 site_page = 1
 hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -31,10 +31,10 @@ while(site_page < 8):
     page = urlopen(req)
     soup = BeautifulSoup(page, 'html.parser') 
 
-    #get elements with value class = entry-title
+    #får elementer med value class = entry-title
     fish = soup.find_all(class_="entry-title")
 
-    #loop through fish and insert data into table
+    #looper gjennom fisk og setter data inn i  table
     for f in fish:
         val = f.get_text().split(", ")
         query = "INSERT INTO fish (name, genus, species) VALUES ('" + val[0] + "', '" + val[1].split(' ',1)[0] + "', '" + val[1].split(' ',1)[1] + "');"
